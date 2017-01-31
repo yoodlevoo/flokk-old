6371cd900f3dcbff16f3f3c3843d6bbcfb1a9a7f
gannonprudhomme <taylorhighschool@Taylors-Mac-mini.local>
2017-01-31 13:56:30 -0600

    Added Group

---
+++ Group.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  Flokk
+//
+//  Created by Taylor High School on 1/27/17.
+//  Copyright © 2017 Heyen Enterprises. All rights reserved.
+//
+
+import Foundation
+
+class Group {
+    var groupName: String
+    var groupIcon: UIImage
+    
+}
