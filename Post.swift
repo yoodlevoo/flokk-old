a4967a221d86357786f0d9b9b2c2bd50157383a2
gannonprudhomme <taylorhighschool@Taylors-Mac-mini.local>
2017-02-07 14:34:44 -0600

    Starting implementing loading comments from JSON files.

--- Post.swift
+++ Post.swift
@@ -15,11 +15,20 @@ class Post {
     var poster: User //the user who posted the image
     var image: UIImage //the image posted
     
-    var comments = [Comment]() //holds all the comments
+    var comments = [Comment]() //holds all the comments, hopefully stored in order
     
     init(poster: User, image: UIImage) {
         self.poster = poster
         self.image = image
+    }
+    
+    //loads the comments from the relevant JSON file
+    //how should we decide how to store each post's comment(ie the file name)
+    func loadComments() {
+        //do {
+        //    if let data = data,
+        //        let json = try JSONSerialization.jsonObject(with: data, )
+        //
     }
 }
 
@@ -28,9 +37,11 @@ class Post {
 class Comment {
     var user: User //the user who posted the comment
     var comment: String //the text of the comment
+    var date: Date
     
     init(user: User, comment: String) {
         self.user = user
         self.comment = comment
+        self.date = Date() //later this needs to be loaded alongside the rest of the data, not created here
     }
 }
