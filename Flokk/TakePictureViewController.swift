87e0f47aa47f83ad4561e285c73deaf673382b82
Jared Heyen <Jared@jareds-macbook-pro.local>
2017-02-28 00:14:14 -0600

    Began working on photo upload view controller, Connected notifications view controller, added search view controller

---
+++ Flokk/TakePictureViewController.swift
@@ -0,0 +1,46 @@
+//
+//  TakePictureViewController.swift
+//  Flokk
+//
+//  Created by Jared Heyen on 2/27/17.
+//  Copyright © 2017 Heyen Enterprises. All rights reserved.
+//
+
+import UIKit
+
+class TakePictureViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet weak var imagePicked: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func openCameraButton(sender: AnyObject) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerControllerSourceType.camera;
+            imagePicker.allowsEditing = false
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
+
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
