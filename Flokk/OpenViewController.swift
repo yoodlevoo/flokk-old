d6cc78e4d3562ee215764283b2d7decab436ec82
Jared Heyen <Jared@jareds-macbook-pro.local>
2017-03-03 23:48:49 -0600

    Began working on new sign in/sign up view controllers

---
+++ Flokk/OpenViewController.swift
@@ -0,0 +1,40 @@
+//
+//  OpenViewController.swift
+//  Flokk
+//
+//  Created by Jared Heyen on 3/3/17.
+//  Copyright © 2017 Heyen Enterprises. All rights reserved.
+//
+
+import UIKit
+
+class OpenViewController: UIViewController {
+    @IBOutlet weak var flokkLogo: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func signUpBttn(_ sender: Any) {
+    }
+    @IBAction func signInPageBttn(_ sender: Any) {
+    }
+
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+}
